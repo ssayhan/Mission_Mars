f08feb945e67d4c98eaa7105b323e1d587deb679
Salih Safa AYHAN <77603561+ssayhan@users.noreply.github.com>
2022-06-05 22:14:05 -0600

    Add files via upload

---
+++ Mission_to_Mars_Challange.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# Import Splinter, BeautifulSoup, and Pandas
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+
+
+# Set the executable path and initialize Splinter
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# ### Visit the NASA Mars News Site
+
+# Visit the mars nasa news site
+url = 'https://redplanetscience.com/'
+browser.visit(url)
+
+# Optional delay for loading the page
+browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+
+# Convert the browser html to a soup object and then quit the browser
+html = browser.html
+news_soup = soup(html, 'html.parser')
+
+slide_elem = news_soup.select_one('div.list_text')
+
+
+slide_elem.find('div', class_='content_title')
+
+
+# Use the parent element to find the first a tag and save it as `news_title`
+news_title = slide_elem.find('div', class_='content_title').get_text()
+print(news_title)
+
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+print(news_p)
+
+
+# ### JPL Space Images Featured Image
+
+# Visit URL
+url = 'https://spaceimages-mars.com'
+browser.visit(url)
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+print(img_soup)
+
+
+# find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+print(img_url_rel)
+
+
+# Use the base url to create an absolute url
+img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+print(img_url)
+
+# ### Mars Facts
+
+df = pd.read_html('https://galaxyfacts-mars.com')[0]
+# df.head()
+
+
+# In[14]:
+
+
+df.columns=['Description', 'Mars', 'Earth']
+df.set_index('Description', inplace=True)
+print(df)
+
+
+df.to_html()
+
+
+# # D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles
+
+# ### Hemispheres
+
+# 1. Use browser to visit the URL 
+url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+browser.visit(url)
+
+
+# 2. Create a list to hold the images and titles.
+hemisphere_image_urls = []
+
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
+for i in range(4):
+    hemispheres = {}
+    
+    # find and click hemisphere page link
+    browser.find_by_css('a.product-item h3')[i].click()
+    
+    # Begin Scraping
+    # look insede to full image url
+    element = browser.find_link_by_text('Sample').first
+    img_link= element['href']
+    
+    #look insde to h2 tag for text
+    img_title = browser.find_by_css("h2.title").text
+    
+    hemispheres["img_link"] = img_url
+    #print(img_url)
+    
+    hemispheres["img_title"] = img_title
+    #print(title)
+    
+    hemisphere_image_urls.append(hemispheres)
+    
+    # go back to url
+    browser.back()
+
+
+# 4. Print the list that holds the dictionary of each image url and title.
+print(hemisphere_image_urls)
+
+
+# 5. Quit the browser
+browser.quit()
+
+
+
---
+++ app.py
@@ -0,0 +1,36 @@
+
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import Mars_scraping
+
+#  Set up Flask
+app = Flask(__name__)
+
+#  Connect tho Mongo using Python
+#  Use Flask_pymongo to set uo mongo connection
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Define the route for HTML
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+#Add next route and function to our code
+
+@app.route("/scrape")
+
+def scrape():
+    mars= mongo.db.mars
+    mars_data = Mars_scraping.scrape_all()
+    mars.update_one({}, {"$set":mars_data}, upsert = True)
+    # mars.update({}, {"$set":mars_data}, upsert=True)
+    return redirect('/', code=302)
+
+# Run the code
+
+if __name__ =="__main__":
+    app.run(host="localhost", port=5001, debug=True)
+
